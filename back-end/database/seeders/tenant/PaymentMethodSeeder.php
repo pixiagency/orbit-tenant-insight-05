8d76499e2fd8f91f9a4d70d21a0c425dcf869185
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2025-09-10 18:54:16 +0200

    fix payment method seeders

--- back-end/database/seeders/tenant/PaymentMethodSeeder.php
+++ back-end/database/seeders/tenant/PaymentMethodSeeder.php
@@ -15,42 +15,42 @@ class PaymentMethodSeeder extends Seeder
         // Default payment methods (pre-defined)
         PaymentMethod::create([
             'name' => 'Cash',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => true,
             'is_manual_added' => false,
         ]);
         
         PaymentMethod::create([
             'name' => 'Bank Transfer',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => false,
             'is_manual_added' => false,
         ]);
         
         PaymentMethod::create([
             'name' => 'Crypto',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => false,
             'is_manual_added' => false,
         ]);
         
         PaymentMethod::create([
             'name' => 'Card',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => false,
             'is_manual_added' => false,
         ]);
         
         PaymentMethod::create([
             'name' => 'Check',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => false,
             'is_manual_added' => false,
         ]);
         
         PaymentMethod::create([
             'name' => 'Paypal',
-            'is_enabled' => true,
+            'is_checked' => true,
             'is_default' => false,
             'is_manual_added' => false,
         ]);
