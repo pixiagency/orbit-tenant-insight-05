3d1dd5a9843323a9b72921aa718f35f3bd0b4f19
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2025-09-18 12:22:37 +0200

    fix issue in appServiceProvider when run command

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -19,10 +19,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->commands([
-            \App\Console\Commands\ProcessTaskEscalations::class,
-            \App\Console\Commands\ProcessTaskReminders::class,
-            \App\Console\Commands\ManageTranslations::class,
-        ]);
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \App\Console\Commands\ProcessTaskEscalations::class,
+                \App\Console\Commands\ProcessTaskReminders::class,
+                \App\Console\Commands\ManageTranslations::class,
+            ]);
+        }
     }
 }
